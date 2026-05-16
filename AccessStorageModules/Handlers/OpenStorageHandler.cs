3762ced013f39c4e962be3584a25df455fbf8827
slin-o <nils@leskms.com>
2026-05-16 20:21:57 +0200

    Initial Commit

---
+++ AccessStorageModules/Handlers/OpenStorageHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static VFXParticlesPool;
+
+namespace AccessStorageModules.Handlers;
+
+internal class OpenStorageHandler
+{
+    public static void OpenStorage(Vehicle vehicle, int slotID)
+    {
+        if (vehicle is Exosuit exo)
+        {
+            exo.storageContainer.Open();
+        }
+        else if (vehicle is SeaMoth seamoth)
+        {
+            seamoth.storageInputs[slotID].OpenPDA();
+        }
+    }
+}
---
+++ AccessStorageModules/Patches/VehiclePatch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccessStorageModules.Handlers;
+using HarmonyLib;
+
+namespace AccessStorageModules.Patches;
+
+[HarmonyPatch(typeof(Vehicle))]
+internal class VehiclePatch
+{
+    [HarmonyPatch(nameof(Vehicle.SlotKeyDown))]
+    [HarmonyPostfix]
+    public static void SlotKeyDownPostfix(Vehicle __instance, int slotID){
+        if (__instance.modules.GetTechTypeInSlot(__instance.slotIDs[slotID]) == TechType.VehicleStorageModule)
+            OpenStorageHandler.OpenStorage(__instance, slotID);
+
+    }
+}
